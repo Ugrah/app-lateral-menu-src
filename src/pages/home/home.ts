1522ef12bad3d946d7d8487ec96310cbee45eced
grulog <lobognonulrichgrah@outlook.fr>
2019-01-07 16:56:38 +0100

    Add homeListLink of HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HomeListLinks} from "../../variables-infos/pagesList";
 
 @Component({
   selector: 'page-home',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {
+    homeListLink: any[] = HomeListLinks;
+
+    constructor() {
   }
 
 }
